5da8aac451582bb7b7447bcf564d7718e890f2c3
Ling Samuel <lingsamuelgrace@gmail.com>
2020-04-24 17:26:04 +0800

    config: typescript

--- webpack.config.js
+++ webpack.config.js
@@ -8,95 +8,95 @@ const { VueLoaderPlugin } = require('vue-loader');
 require("@babel/polyfill");
 
 module.exports = {
-  mode: "development",
-  entry: {
-    app: ['@babel/polyfill'],
-    main: ['./src/main.js'],
-    background: './src/background.js',
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    // publicPath: '/dist/',
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      }, {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {}
-          // other vue-loader options go here
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-        {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'url-loader'
-        }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+    mode: "development",
+    entry: {
+        app: ['@babel/polyfill'],
+        main: ['./src/main.js'],
+        background: './src/background.js',
     },
-    extensions: ['*', '.js', '.vue', '.json']
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    overlay: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'src/manifest.json',
-        to: 'manifest.json',
-      },
-      { from: 'src/assets', to: 'assets' },
-    ]),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-      excludeChunks: ['background'], // Do not add to index.html
-      inject: true,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: false
-    }),
-    new VueLoaderPlugin(),
-  ]
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        // publicPath: '/dist/',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            }, {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {}
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.(js|ts)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json', '.ts']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'src/manifest.json',
+                to: 'manifest.json',
+            },
+            { from: 'src/assets', to: 'assets' },
+        ]),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+            excludeChunks: ['background'], // Do not add to index.html
+            inject: true,
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: false
+        }),
+        new VueLoaderPlugin(),
+    ]
 };
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map';
-  module.exports.mode = "production";
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([])
+    module.exports.devtool = '#source-map';
+    module.exports.mode = "production";
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([])
 }
